e53efd04e332eb66d47965f3bd3c7f93804377ea
sivakumarrkrishnan <50791113+sivakumarrkrishnan@users.noreply.github.com>
2019-05-20 19:49:56 +0530

    Add files via upload
    updated after attending the review comments(pep8 guidelines)

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,26 +1,31 @@
 import fresh_tomatoes
 import media
 
-# creating an instance named "avengers" for the Movie class and instantiating the movie objects
-avengers = media.Movie("Avengers Endgame", \
-                        "https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg", \
-                        "https://www.youtube.com/watch?v=TcMBFSGVi1c")
+# creating an instance named "avengers" for the Movie class and
+# instantiating the movie objects
+avengers = media.Movie("Avengers Endgame",
+                       "https://upload.wikimedia.org/wikipedia/en/0/0d/"
+                       "Avengers_Endgame_poster.jpg",
+                       "https://www.youtube.com/watch?v=TcMBFSGVi1c")
 
 
 # creating an instance named "dumbo" for the Movie class
-dumbo = media.Movie("Dumbo",  \
-                        "https://upload.wikimedia.org/wikipedia/en/9/91/Dumbo_%282019_film%29.png", \
-                        "https://www.youtube.com/watch?v=7NiYVoqBt-8")
+dumbo = media.Movie("Dumbo",
+                    "https://upload.wikimedia.org/wikipedia/en/9/91/"
+                    "Dumbo_%282019_film%29.png",
+                    "https://www.youtube.com/watch?v=7NiYVoqBt-8")
 
 # creating an instance named "how_to_train" for the Movie class
-how_to_train = media.Movie("How to Train Your Dragon",  \
-                        "https://upload.wikimedia.org/wikipedia/en/f/fd/How_to_Train_Your_Dragon_3_poster.png", \
-                        "https://www.youtube.com/watch?v=CQ7XUCQ6pbE")
+how_to_train = media.Movie("How to Train Your Dragon",
+                           "https://upload.wikimedia.org/wikipedia/en/f/fd/"
+                           "How_to_Train_Your_Dragon_3_poster.png",
+                           "https://www.youtube.com/watch?v=CQ7XUCQ6pbE")
 
 # creating an instance named "captain_marvel" for the Movie class
-captain_marvel = media.Movie("Captain Marvel",  \
-                        "https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg", \
-                        "https://www.youtube.com/watch?v=Z1BCujX3pw8")
+captain_marvel = media.Movie("Captain Marvel",
+                             "https://upload.wikimedia.org/wikipedia/en/8/85/"
+                             "Captain_Marvel_poster.jpg",
+                             "https://www.youtube.com/watch?v=Z1BCujX3pw8")
 
 # storing all the instances created in a list
 movies = [avengers, dumbo, how_to_train, captain_marvel]
